45ce8a84c1167fc08409b1ea9affbdfe9edf8b7a
Beth Schofield <bethmschofield@gmail.com>
2018-03-22 05:18:24 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -11,17 +11,17 @@ def create_project_hash
 
   kickstarter.css(".project-card").each do |project|
     projects[project] = {}
-    
+
 end
 
 
 projects
 
 # Flatiron says: projects: kickstarter.css("li.project.grid_4")
-# I think:
-projects: kickstarter.css(".project-card")
-title: project.css("h2.bbcard_name strong a").text
-imagelink: project.css(".project-thumbnail a img").attribute("src").value
-description: project.css("p.bbcard_blurb").text
-location: project.css("ul.project-meta li a span.location-name").text
-percentagefunded: project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
+# # I think:
+# projects: kickstarter.css(".project-card")
+# title: project.css("h2.bbcard_name strong a").text
+# imagelink: project.css(".project-thumbnail a img").attribute("src").value
+# description: project.css("p.bbcard_blurb").text
+# location: project.css("ul.project-meta li a span.location-name").text
+# percentagefunded: project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
