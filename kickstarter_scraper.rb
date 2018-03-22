2e3a0ca970bd1adc0c0db3233e03fcc343854476
Beth Schofield <bethmschofield@gmail.com>
2018-03-22 04:10:05 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -11,3 +11,10 @@ end
 
 
 create_project_hash
+
+# Flatiron says: projects: kickstarter.css("li.project.grid_4")
+# I think: 
+projects: kickstarter.css(".project-card")
+project.css(".project-thumbnail a img")
+title: project.css("h2.bbcard_name strong a").text
+imagelink: project.css(".project-thumbnail a img").attribute("src").value
