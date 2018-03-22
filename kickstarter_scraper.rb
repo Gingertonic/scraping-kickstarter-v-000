cc691f3a4d7bbbf03b5059919fa0c4876398d7b6
Beth Schofield <bethmschofield@gmail.com>
2018-03-22 03:56:04 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,6 +1,9 @@
 # require libraries/modules here
 require 'nokogiri'
 
+html = File.read('fixtures/kickstarter.html')
+kickstarter = Nokogiri::HTML(open(html))
+
 def create_project_hash
   # write your code here
 end
